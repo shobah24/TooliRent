5427dbf88afcccee258e65bd56e813784b359827
shobah24 <shokran.bahram14@gmail.se>
2025-09-25 16:06:27 +0200

    validator for category

---
+++ TooliRent.Application/Validators/CreateCategoryDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using TooliRent.Application.Dto.Category;
+
+namespace TooliRent.Application.Validators
+{
+    public class CreateCategoryDtoValidator : AbstractValidator<CreateCategoryDto>
+    {
+        public CreateCategoryDtoValidator() 
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Kategorinamn får inte vara tomt.")
+                .MaximumLength(100).WithMessage("Kategorinamn får inte överstiga 100 tecken.");
+
+        }
+    }
+}
