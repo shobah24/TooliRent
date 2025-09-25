4eb4235d482542d2374072395798e54d6eb4049d
shobah24 <shokran.bahram14@gmail.se>
2025-09-25 16:05:42 +0200

    Fixed dto for category without tools

--- TooliRent.Application/Dto/Category/CategoryDto.cs
+++ TooliRent.Application/Dto/Category/CategoryDto.cs
@@ -1,8 +1,12 @@
-﻿namespace TooliRent.Application.Dto.Category
+﻿using TooliRent.Application.Dto.Tool;
+
+namespace TooliRent.Application.Dto.Category
 {
     public class CategoryDto
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+
+        public IEnumerable<ToolDto> Tools { get; set; } = new List<ToolDto>();
     }
 }
---
+++ TooliRent.Application/Dto/Category/CategoryWithoutToolsDtos.cs
@@ -0,0 +1,8 @@
+﻿namespace TooliRent.Application.Dto.Category
+{
+    public class CategoryWithoutToolsDtos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
