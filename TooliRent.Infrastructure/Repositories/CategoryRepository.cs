d2f3b62320e17f21da1d0f7f797858db68cc18bb
shobah24 <shokran.bahram14@gmail.se>
2025-09-25 16:04:25 +0200

    CategoryRepo fixed

---
+++ TooliRent.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TooliRent.Domain.Models;
+using TooliRent.Domain.RepoInterfaces;
+using TooliRent.Infrastructure.Data;
+
+namespace TooliRent.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly TooliRentDbContext _context;
+
+        public CategoryRepository(TooliRentDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetCategoryByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task<Category?> GetCategoryWithToolsAsync(int id)
+        {
+            return await _context.Categories
+                .Include(c => c.Tools)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+        public async Task AddCategoryAsync(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync(); 
+        }
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteCategoryAsync(Category category)
+        {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
